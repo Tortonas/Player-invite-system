6ca9f209a913e8d27dedf47b33db8ed3cef39653
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-12-20 17:07:32 +0200

    Transferred all of the plain HTML design to twig, created symfony skeleton, added admin bundle.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
