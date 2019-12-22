89ae749467c21c25a22a092259604e09214b1429
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-12-22 19:10:32 +0200

    Password change form

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -11,12 +14,41 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile", name="app_profile")
      * @Security("is_granted('ROLE_ADMIN')")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if($request->isMethod('POST'))
+        {
+            /** @var User $loggedUser */
+            $loggedUser = $this->getUser();
+            if(password_verify($request->request->get('oldPassword'), $loggedUser->getPassword()))
+            {
+                if($request->request->get('newPassword') == $request->request->get('newPassword2'))
+                {
+                    $loggedUser->setPlainPassword($request->request->get('newPassword'));
+
+                    $entityManager->persist($loggedUser);
+                    $entityManager->flush();
+
+                    $this->addFlash('success', "Your password was succesfully changed!");
+                }
+                else
+                {
+                    $this->addFlash('danger', "Your new passwords do not match!");
+                }
+            }
+            else
+            {
+                $this->addFlash('danger', "Your old password is wrong!");
+            }
+        }
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-            'user' => $this->getUser()
+            'user' => $this->getUser(),
         ]);
     }
 }
